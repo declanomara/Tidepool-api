b17c1ef812523fba2d1836a842e77fc7f769daf5
declanomara <25646795+declanomara@users.noreply.github.com>
2022-04-26 14:12:07 -0700

    Updated scripts to use python 3.10.

--- install/restart_api.sh
+++ install/restart_api.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 cd /usr/share/Tidepool-frontend &&
-  uvicorn main:app --host 0.0.0.0 \
+  python3.10 -m uvicorn main:app --host 0.0.0.0 \
                    --port 443 \
                    --ssl-keyfile=/etc/letsencrypt/live/api.tidepool.finance/privkey.pem \
                    --ssl-certfile=/etc/letsencrypt/live/api.tidepool.finance/fullchain.pem
