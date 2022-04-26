25366e4a60579cbaf8dc69f3e73c9e0d8b6339fa
declanomara <25646795+declanomara@users.noreply.github.com>
2022-04-25 23:08:12 -0700

    Created install script.

---
+++ install/install.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+sudo pip3 install -r requirements.txt
+
+sudo cp install/services/*.service /lib/systemd/system/
+mkdir /usr/share/Tidepool-frontend
+sudo cp -r * /usr/share/Tidepool-frontend/
+
+sudo systemctl daemon-reload
+sudo systemctl enable tidepool-api.service
