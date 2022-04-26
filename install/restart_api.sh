ea2902b2572e23ce8d6d4829affd6c08a237a102
declanomara <25646795+declanomara@users.noreply.github.com>
2022-04-25 23:11:56 -0700

    Created install script.

--- install/restart_api.sh
+++ install/restart_api.sh
@@ -3,5 +3,5 @@
 cd /usr/share/Tidepool-frontend &&
   uvicorn main:app --host 0.0.0.0 \
                    --port 443 \
-                   --ssl-privkey=/etc/letsencrypt/live/api.tidepool.finance/privkey.pem \
+                   --ssl-keyfile=/etc/letsencrypt/live/api.tidepool.finance/privkey.pem \
                    --ssl-certfile=/etc/letsencrypt/live/api.tidepool.finance/fullchain.pem
