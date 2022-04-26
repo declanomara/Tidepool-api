b2de40324e2380e275565deaa34d7155b293c4d4
declanomara <25646795+declanomara@users.noreply.github.com>
2022-04-25 19:08:15 -0700

    Initial version of API

---
+++ main.py
@@ -0,0 +1,46 @@
+import pymongo
+import helpers
+
+from fastapi import FastAPI, HTTPException
+
+
+app = FastAPI()
+
+VERSION = 1.0
+cfg = helpers.load_config('cfg.ini')
+client = pymongo.MongoClient(cfg['db_string'])
+tidepool_db = client['tidepool']
+tidepool_stats_db = client['tidepool-stats']
+
+
+@app.get('/v1/')
+def root():
+
+    info = {
+        'api_version': VERSION,
+        'db_version': tidepool_db.command({'buildInfo': 1})['version']
+    }
+
+    return info
+
+
+@app.get('/v1/stats/latest/')
+def all_latest_stats():
+    data_rates = tidepool_stats_db['latest'].find({'type': 'data_speed'})
+
+    response = {}
+    for stat in data_rates:
+        response[stat['instrument']] = {'data_rate': stat['data_rate']}
+
+    return response
+
+
+@app.get('/v1/stats/latest/{instrument}')
+def latest_stats(instrument):
+    all_stats = all_latest_stats()
+
+    if instrument in all_stats:
+        return all_stats[instrument]
+
+    else:
+        raise HTTPException(status_code=404, detail=f"Instrument '{instrument}' not found.")
