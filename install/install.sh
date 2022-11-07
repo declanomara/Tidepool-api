f967ccf8d304730077e2fcacb613639d25084328
declanomara <25646795+declanomara@users.noreply.github.com>
2022-11-06 18:45:09 -0800

    Improved installation script for greater feedback and virtual environment utilization.

--- install/install.sh
+++ install/install.sh
@@ -1,11 +1,75 @@
 #!/usr/bin/env bash
 
-sudo pip3 install -r requirements.txt
+# Ensure running as root
+if [ "$EUID" -ne 0 ]
+    then echo "Install script must be run as root."
+    exit
+fi
 
+
+INSTALL_DIR="/usr/share/Tidepool-frontend/"
+
+
+# Check if in virtual environment already
+if [[ "$VIRTUAL_ENV" != "" ]]
+then
+  # In virtual environment
+  echo "Already running in virtual environment."
+
+else
+  # Not in virtual environment
+
+  # Check if in virtual environment exists
+  if [ -d "./venv" ]
+  then
+    echo "Virtual environment already exists."
+  else
+    # Create virtual environment
+    echo "Creating virtual environment..."
+    python3.10 -m venv ./venv
+    echo "Virtual environment created."
+  fi
+
+  # Activate virtual environment
+  echo "Activating virtual environment..."
+  source "./venv/bin/activate"
+  echo "Virtual environment activated."
+fi
+
+
+# Install python requirements
+echo "Installing python requirements..."
+python3 -m pip install -r requirements.txt
+echo "Done."
+
+# Move services into systemd directory
+echo "Installing systemd services..."
 sudo cp install/services/*.service /lib/systemd/system/
-mkdir /usr/share/Tidepool-frontend
-sudo cp -r * /usr/share/Tidepool-frontend/
+echo "Done."
 
-sudo systemctl daemon-reload
-sudo systemctl restart tidepool-api.service
-sudo systemctl enable tidepool-api.service
+# Move program files into correct directory
+if [ -d $INSTALL_DIR ]
+then
+  echo "Install directory already exists."
+else
+  echo "Creating install directory: $INSTALL_DIR"
+  mkdir $INSTALL_DIR
+  echo "Created directory $INSTALL_DIR."
+fi
+
+echo "Moving required files to $INSTALL_DIR"
+sudo cp -r ./* $INSTALL_DIR
+
+# Reload and start new services
+
+echo "Reloading systemd..."
+systemctl daemon-reload
+echo "Done."
+
+echo "Starting Tidepool services..."
+systemctl restart tidepool-api.service
+echo "Done."
+
+echo "Enabling Tidepool services..."
+systemctl enable tidepool-api.service
+echo "Done."
