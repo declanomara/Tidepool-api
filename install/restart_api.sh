94cc7b1c05a7c9a76aa971e12931d4fbac258a9a
declanomara <25646795+declanomara@users.noreply.github.com>
2022-11-06 18:48:56 -0800

    Improved API IPC coverage and added status endpoints.

--- install/restart_api.sh
+++ install/restart_api.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-cd /usr/share/Tidepool-frontend &&
+
+cd /usr/share/Tidepool-api && source /usr/share/Tidepool-api/venv/bin/activate &&
   python3 -m uvicorn main:app --host 0.0.0.0 \
                    --port 443 \
                    --ssl-keyfile=/etc/letsencrypt/live/api.tidepool.finance/privkey.pem \
